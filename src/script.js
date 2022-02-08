6d92f1590255ee3b305bf36da636dd19840ca322
TanveerCedcoss <tanveerhusain@cedcoss.com>
2022-02-08 12:51:48 +0530

    Improvised

--- src/script.js
+++ src/script.js
@@ -1,29 +1,64 @@
 var list=[];
+
 function fetchData()
 {
-    var product=insert();
-    list.push(product);
-    table =document.getElementById("table");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    for (var i=0; i<list.length; i++){
-        cell1.innerHTML = list[i].pId;
-        cell2.innerHTML = list[i].pName;
-        cell3.innerHTML = list[i].pPrice;
-    }
+    insertData();
+    console.log(list);
+    display();
+    
 }
 
 
-function insert()
+function insertData()
 {
     var id= document.getElementById("productId").value;
     var name= document.getElementById("productName").value;
     var price= document.getElementById("productPrice").value;
-    return{
-        "pId":id,
-        "pName":name,
-        "pPrice":price 
+    var exist=checkData(id);
+    if(exist==true)
+    {
+        alert('ID already exist!!!!');
     }
+    else
+    {
+        var data={
+            "pId":id,
+            "pName":name,
+            "pPrice":price 
+        }
+        list.push(data);
+    }
+    
+}
+
+function display()
+{
+    var text = document.getElementById("table")
+    text.innerHTML="<tr>\
+    <th>Product Id</th>\
+    <th>Product Name</th>\
+    <th>Product Price</th>\
+  </tr>";
+  for(var i=0;i<list.length;i++)
+  {
+      text.innerHTML+="<tr>\
+      <td>"+list[i].pId+"</td>\
+      <td>"+list[i].pName+"</td>\
+      <td>"+list[i].pPrice+"</td>\
+      </tr>";
+  }
+}
+
+function checkData(id)
+{
+    console.log('data check done') ;
+    for (var i=0; i<list.length; i++)
+    {
+        if(id == list[i].pId)
+        {
+            return true;
+
+        }
+    }
+    
 }
